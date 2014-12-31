aa0b6ff2e34667db01d4ab562894bd66c46a8505
Duncan Burggraf <dcburggraf@gmail.com>
2014-12-30 23:30:35 -0800

    added colors support; text is now green on black

--- nclock_main.c
+++ nclock_main.c
@@ -6,48 +6,60 @@ static void window_clear(WINDOW * win, int width, int height);
 
 int main(void)
 {
-    initscr();
-    cbreak();
-    keypad(stdscr, TRUE);
-    noecho();
+    initscr(); // start curses mode
+    halfdelay(1); // pause looking for a tenth when looking for input
+    keypad(stdscr, TRUE); // allow input from keyboard
+    noecho(); // turn off echoing key presses
+    curs_set(0); // turn off cursor
+
+    // setup color green on black
+    start_color();
+    init_pair( 1, COLOR_GREEN, COLOR_BLACK );
+    attrset( COLOR_PAIR(1) );
+    
 
     int rowc, colc, xmid, ymid, x0, y0, height = 5, width = 31;
-
     getmaxyx(stdscr,rowc,colc);
     xmid = colc / 2;
     ymid = rowc / 2;
 
     // window dimensions are 31x5 so x0 = xmid - 15, y0 = wmid - 2
     x0 = xmid - 15;
-    y0 = ymid - 15;
+    y0 = ymid - 2;
     if (x0 < 0 || y0 < 0)
     {
         endwin();
+        printw("Your terminal window is %ix%i.\n", colc, rowc);
         printw("Increase your window size to at least 31x5 to view clock\n");
         printw("Press any key to quit");
         getch();
+        endwin();
         return 0;
     }
 
     // init clock window
     WINDOW * win = newwin(height, width, y0, x0);
+    wattrset(win, COLOR_PAIR(1));
 
-    //int ch;
+    int ch;
     time_t epochtime = time(NULL);
-    while (1)
+    while ( (ch = getch()) != KEY_F(2) )
     {
-        //delay to next clock tick (or F1 key for quit)
-        while ( epochtime == time(NULL) );
-
-        //quit if we get the F1 key
-        //if (ch == KEY_F(1)) return 0;
-
-        //clear window, then display time
-        window_clear(win,width,height);
-        nclock_display(win, nclock_getTime());
-        refresh();
+        if (epochtime != time(NULL))
+        {
+            //clear window, then display time
+            window_clear(win,width,height);
+            nclock_display(win, nclock_getTime());
+            refresh();
+            epochtime = time(NULL);
+        }
     }
 
+    attroff(COLOR_PAIR(1));
+    wattroff(win, COLOR_PAIR(1));
+    endwin();
+
+    return 0;
 
 }
 
